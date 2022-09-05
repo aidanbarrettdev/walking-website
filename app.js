5addb60ee4b0c4fef95746ff6776736fc80c2026
Aidan Barrett <aidbarrett94@gmail.com>
2022-09-05 18:59:56 +0100

    mobile nav

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("working");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
+
+const hamburgerToggle = () => {
+  hamburgerMenu.addEventListener("click", () => {
+    nav.classList.toggle("nav-toggle");
+    console.log("confirm");
+  });
+};
+hamburgerToggle();
