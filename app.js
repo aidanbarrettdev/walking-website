9cdac4e434e1d65c6acba6d3ae9902fe1527d077
Aidan Barrett <aidbarrett94@gmail.com>
2022-09-15 16:56:32 +0100

    added form html/ js and css for mobile

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const nav = document.querySelector(".nav");
 const APIKEY = "aecc5d608d6e04ebfaf220bbc970f79e";
 const weatherInput = document.querySelector(".weather-input");
 const weatherInputBtn = document.querySelector(".weather-input-btn");
-
+const firstName = document.getElementById("first-name");
+const secondName = document.getElementById("second-name");
+const form = document.querySelector(".form");
+const errorDisplay = document.querySelector(".display-error");
+const formPassword = document.getElementById("password");
+const formConfirmationPassword = document.getElementById("confrim-password");
+//navigation
 const hamburgerToggle = () => {
   hamburgerMenu.addEventListener("click", () => {
     nav.classList.toggle("nav-toggle");
@@ -12,6 +18,7 @@ const hamburgerToggle = () => {
 };
 hamburgerToggle();
 
+//weather api
 weatherInputBtn.addEventListener("click", searchWeather);
 
 function searchWeather(e) {
@@ -39,3 +46,23 @@ function displayInfo(data) {
   );
   weatherDiscription.innerHTML = `${data.weather[0].description} <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
 }
+//form
+
+form.addEventListener("submit", (e) => {
+  let errorMessages = [];
+  if (formPassword.value.length <= 5) {
+    errorMessages.push("Password must be longer than 5 characters");
+  }
+  if (formPassword.value.length >= 12) {
+    errorMessages.push("Password must be less than 12 characters");
+  }
+
+  if (formPassword.value !== formConfirmationPassword.value) {
+    errorMessages.push("passwords must match");
+  }
+  if (errorMessages.length > 0) {
+    e.preventDefault();
+    form.classList.add("form-error");
+    errorDisplay.innerText = errorMessages;
+  }
+});
